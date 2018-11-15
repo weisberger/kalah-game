74cb653392eacb3edf8d9a1da55502a4de94c9da
sari weisberger <208sari@gmail.com>
2018-11-15 20:36:52 +0200

    initialization Player 2 crosses test

--- test_kalah.py
+++ test_kalah.py
@@ -28,6 +28,20 @@ class KalahTestCase(unittest.TestCase):
         game.play('E')
         self.assertEqual(game.status(), (4, 0, 5, 5, 5, 5, 0, 4, 0, 5, 5, 5, 5, 0))
 
+    def test_Player2crosses(self):
+        game = Kalah(6, 4)
+        game.play('e')
+        self.assertEqual(game.status(), (4, 0, 5, 5, 5, 5, 0, 4, 4, 4, 4, 4, 4, 0))
+        game.play('E')
+        self.assertEqual(game.status(), (4, 0, 5, 5, 5, 5, 0, 4, 0, 5, 5, 5, 5, 0))
+        game.play('a')
+        self.assertEqual(game.status(), (4, 0, 5, 5, 5, 0, 1, 5, 1, 6, 6, 5, 5, 0))
+        game.play('A')
+        self.assertEqual(game.status(), (5, 1, 6, 6, 5, 0, 1, 5, 1, 6, 6, 5, 0, 1))
+
+
+
+
 
 
 
