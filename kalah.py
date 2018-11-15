6dd5442787675c4f69b0e12f9ee36bbb78d35cc0
sari weisberger <208sari@gmail.com>
2018-11-15 19:38:38 +0200

    initialization simple move test

--- kalah.py
+++ kalah.py
@@ -5,14 +5,21 @@ class Kalah(object):
         self.seeds = seeds
         lettersLowercase = string.ascii_lowercase
         lettersUppercase = string.ascii_uppercase
-        dictLowercase = dict(zip(lettersLowercase, [self.seeds]*holes))
-        dictUppercase = dict(zip(lettersUppercase, [self.seeds]*holes))
+        dictLowercase = dict(list(zip(lettersLowercase, [self.seeds]*holes))[::-1])
+        dictUppercase = dict(list(zip(lettersUppercase, [self.seeds]*holes))[::-1])
+
         self.board = {**dictLowercase, 'bankPlayerone': 0, **dictUppercase, 'bankPlayertow': 0}
         self.turn = (1, 0)
 
     def play(self, hole):
         if self.turn[0] == 1 and hole.isupper():
             raise IndexError
+        current = ord(hole)
+        for i in range(self.board[hole]):
+            self.board[chr(current - 1)] += 1
+            current -= 1
+        self.board[hole] = 0
+
         return
 
     def status(self):
--- test_kalah.py
+++ test_kalah.py
@@ -10,6 +10,11 @@ class KalahTestCase(unittest.TestCase):
     def test_Illegalhole(self):
         game = Kalah(6, 4)
         self.assertRaises(IndexError, game.play, 'A')
+    def test_Simplemove(self):
+        game = Kalah(6, 4)
+        game.play('f')
+        self.assertEqual(game.status(), (0,5,5,5,5,4,0,4,4,4,4,4,4,0))
+
 
 
 
