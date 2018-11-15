d7ee035d4656868db2e70a05b7c04ef4af45cea7
sari weisberger <208sari@gmail.com>
2018-11-15 21:39:26 +0200

    initialization order part 2 test

--- test_kalah.py
+++ test_kalah.py
@@ -24,18 +24,14 @@ class KalahTestCase(unittest.TestCase):
     def test_TwosimpleMoves(self):
         game = Kalah(6, 4)
         game.play('e')
-        self.assertEqual(game.status(), (4, 0, 5, 5, 5, 5, 0, 4, 4, 4, 4, 4, 4, 0))
         game.play('E')
         self.assertEqual(game.status(), (4, 0, 5, 5, 5, 5, 0, 4, 0, 5, 5, 5, 5, 0))
 
     def test_Player2crosses(self):
         game = Kalah(6, 4)
         game.play('e')
-        self.assertEqual(game.status(), (4, 0, 5, 5, 5, 5, 0, 4, 4, 4, 4, 4, 4, 0))
         game.play('E')
-        self.assertEqual(game.status(), (4, 0, 5, 5, 5, 5, 0, 4, 0, 5, 5, 5, 5, 0))
         game.play('a')
-        self.assertEqual(game.status(), (4, 0, 5, 5, 5, 0, 1, 5, 1, 6, 6, 5, 5, 0))
         game.play('A')
         self.assertEqual(game.status(), (5, 1, 6, 6, 5, 0, 1, 5, 1, 6, 6, 5, 0, 1))
 
@@ -56,29 +52,9 @@ class KalahTestCase(unittest.TestCase):
     def test_EmptyHole(self):
         game = Kalah(6, 4)
         game.play('e')
-        self.assertEqual(game.status(), (4, 0, 5, 5, 5, 5, 0, 4, 4, 4, 4, 4, 4, 0))
         game.play('A')
-        self.assertEqual(game.status(), (5, 1, 6, 5, 5, 5, 0, 4, 4, 4, 4, 4, 0, 1))
         game.play('f')
-        self.assertEqual(game.status(), (0, 2, 7, 6, 6, 6, 0, 4, 4, 4, 4, 4, 0, 1))
         self.assertRaises(ValueError, game.play, 'A')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
