9cba7429bf31a29475283158042a669dc6e8297c
sari weisberger <208sari@gmail.com>
2018-11-15 18:16:53 +0200

    initialization status test

--- kalah.py
+++ kalah.py
@@ -1,2 +1,24 @@
+import string
 class Kalah(object):
-    pass
+    def __init__(self, holes, seeds):
+        self.holes = holes
+        self.seeds = seeds
+        lettersLowercase = string.ascii_lowercase
+        lettersUppercase = string.ascii_uppercase
+        dictLowercase = dict(zip(lettersLowercase, [self.seeds]*holes))
+        dictUppercase = dict(zip(lettersUppercase, [self.seeds]*holes))
+        self.board = {**dictLowercase, 'bankPlayerone': 0, **dictUppercase, 'bankPlayertow': 0}
+
+    def play(self):
+        return
+
+    def status(self):
+        return tuple(self.board.values())
+
+
+    def done(self):
+        return
+
+
+    def score(self):
+        return
--- test_kalah.py
+++ test_kalah.py
@@ -1,8 +1,14 @@
 import unittest
-
+from kalah import Kalah
 
 class KalahTestCase(unittest.TestCase):
-    pass
+
+    def test_InitialStatus(self):
+        game = Kalah(6, 4)
+        self.assertEqual(game.status(), (4,4,4,4,4,4,0,4,4,4,4,4,4,0))
+
+
+
 
 
 if __name__ == '__main__':
