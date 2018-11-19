83ab24beba7a6be154af0229a0aa818088c4094f
sari weisberger <208sari@gmail.com>
2018-11-19 18:59:22 +0200

    "refactor my program"

--- kalah.py
+++ kalah.py
@@ -16,40 +16,37 @@ class Kalah(object):
             raise IndexError
         if self.board[hole] == 0:
             raise ValueError
-        current = ord(hole) - 1
+        current = hole
         i = 0
         flag = 0
-        while i < self.board[hole]:
-            if current == 96:
+        sumOfseeds = self.board[hole]
+        self.board[hole] = 0
+        while i < sumOfseeds:
+            if current == 'a':
                 if self.turn[0] == 1:
                     self.board['bankPlayerone'] += 1
-                    if self.board[hole] - i -1 == 0:
-                        flag = 1
-                else:
-                   i -= 1
-                current = 70
-            elif current == 64:
-                if self.turn[1] == 1:
-                    self.board['bankPlayertow'] += 1
-                    if self.board[hole] - i - 1 == 0:
+                    if sumOfseeds - i - 1 == 0:
                         flag = 1
                 else:
                     i -= 1
-                current = 102
+                current = 'G'
+            elif current == 'A':
+                if self.turn[1] == 1:
+                    self.board['bankPlayertow'] += 1
+                    if sumOfseeds - i - 1 == 0:
+                        flag = 1
+                else:
+                    i -= 1
+                current = 'g'
             else:
-                self.board[chr(current)] += 1
-                current -= 1
+                current = chr(ord(current) - 1)
+                self.board[current] += 1
             i += 1
-        self.board[hole] = 0
         if not flag:
             temp = self.turn[0]
             self.turn[0] = self.turn[1]
             self.turn[1] = temp
         return f"Player {1 if self.turn[0] == 1 else 2} plays next"
-
-
-
-
 
         return
 
