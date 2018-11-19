7c72106f90354132d4839f11bf848c785c4abc2b
sari weisberger <208sari@gmail.com>
2018-11-20 00:48:04 +0200

    "change CapturePlayer1"

--- kalah.py
+++ kalah.py
@@ -42,6 +42,20 @@ class Kalah(object):
                 current = chr(ord(current) - 1)
                 self.board[current] += 1
             i += 1
+        if flag != 1:
+            if self.board[chr(ord(current))] == 1:
+                if current.isupper() and self.turn[1]:#if the seeds were finished when he reached an empty hole that belonged to him
+                    temp = self.holes - (ord(current) - ord('A')) #Calculates the number of holes up to the current one
+                    Parallel = chr(ord('a') + temp) #Calculates the hole parallel to the current hole
+                    self.board['bankPlayertow'] += self.board[Parallel ] + 1
+                if current.islower() and self.turn[0] == 1:#if the seeds were finished when he reached an empty hole that belonged to him
+                    temp = self.holes - (ord(current) - ord('a') + 1) #Calculates the number of holes up to the current one
+                    Parallel = chr(ord('A') + temp) #Calculates the hole parallel to the current hole
+                    self.board['bankPlayerone'] += self.board[Parallel] + 1
+                    self.board[Parallel] = 0
+                    self.board[current] = 0
+
+
         if not flag:
             temp = self.turn[0]
             self.turn[0] = self.turn[1]
